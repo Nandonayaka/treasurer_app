620c95634bac448acfab4ed3f63d9bc498e0c297
nando <ham280207@gmail.com>
2026-05-23 18:16:55 +0700

    feat: title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Kasly - Solusi Kelola Uang Kas Modern</title>
+        <title>Kasly - Solusi Kelola Uang Kas Moderen</title>
+        <link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;500;600;700;800&family=Outfit:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
